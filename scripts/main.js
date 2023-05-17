5bfba063be138620cbfbda3cd365031ab53b7009
Alan <tapia.alan.g@gmail.com>
2023-05-17 00:56:40 -0300

    refactor design

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,49 @@
-window.addEventListener(
-  "scroll",
-  function () {
-    const header = document.querySelector("header");
-    header.classList.toggle("sticky", window.scrollY > 120);
-  },
-  { passive: true }
-);
+// window.addEventListener(
+//   "scroll",
+//   function () {
+//     const header = document.querySelector("header");
+//     header.classList.toggle("sticky", window.scrollY > 120);
+//   },
+//   { passive: true }
+// );
+
+// document.addEventListener( 'DOMContentLoaded', function () {
+
+// } );
 
 window.addEventListener("DOMContentLoaded", () => {
   document.querySelector(".saltarina").classList.remove("opacity-0");
+
+  const splide = new Splide(".splide", {
+    // Optional parameters
+    // start: 1,
+    // perPage: 3,
+    // perMove: 1,
+    // gap: 20,
+    // padding: "5rem",
+    // type: "loop",
+    // drag: "free",
+    // snap: true,
+    // interval: 200,
+    // arrows: true,
+    // pagination: true,
+    // rewind: true,
+    // rewindByDrag: true,
+    // lazyLoad: true,
+    type: "loop",
+    padding: "5rem",
+    gap: 20,
+
+    // Responsive breakpoint
+    breakpoints: {
+      768: {
+        perPage: 1,
+        snap: true,
+      },
+    },
+  });
+
+  splide.mount();
 });
 
 AOS.init({
